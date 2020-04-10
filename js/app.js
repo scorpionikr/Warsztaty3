05e98426f3099b9005ad8565a624b1b2a1788920
Rafal Bartkowski <scorpionikr@gmail.com>
2020-04-10 14:38:39 +0200

    Poprawienie wyswietlania i menu

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const hamburger = document.querySelector('.hamburger button');
     const menu = document.querySelector('.list__navigation');
+    const menu_list = document.querySelectorAll('nav > ul > li ');
     hamburger.addEventListener('click', function(){
         menu.classList.toggle('visible');
         menu.classList.toggle('unvisible');
@@ -10,5 +11,12 @@ document.addEventListener("DOMContentLoaded", function () {
         if (mobile.matches) {
             menu.classList.remove('visible');
         }
+    });
+    //menu hamburger
+    menu_list.forEach(function(element) {
+        element.addEventListener('click', function(){
+            menu.classList.toggle('visible');
+            menu.classList.toggle('unvisible');
+        });
     });
 });
